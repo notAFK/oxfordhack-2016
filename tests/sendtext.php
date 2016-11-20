da75deaedd2c7e8c3043c130fc89bb654e81b3dc
Alexandru-Paul Copil <alexandru-paul.copil@student.manchester.ac.uk>
2016-11-20 08:54:55 +0000

    Remove tests.

--- tests/sendtext.php
+++
@@ -1,3 +0,0 @@
-POST https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment
-POST https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/keyPhrases
-POST https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/languages
--- tests/test_face.py
+++
@@ -1,8 +0,0 @@
-import cognitive_face as CF
-
-KEY = str(raw_input('KEY: '))
-CF.Key.set(KEY)
-IMAGE = 'img.jpg'
-RESULT = CF.face.detect(IMAGE)
-
-print RESULT
