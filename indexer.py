a80d0911ec5bf3f016ef31f0fce4c90fbdb7a432
Alexandru-Paul Copil <alexandru-paul.copil@student.manchester.ac.uk>
2016-11-20 08:19:39 +0000

    Indexer parse lyrics test.

--- indexer.py
+++ indexer.py
@@ -1,5 +1,6 @@
 import os
 import re
+import sys
 import unicodedata
 from makeuplink import get_sentiment
 
@@ -17,6 +18,13 @@ def passfile(filename):
     os.rename(DATAPATH+filename, DATAPATH+newfilename)
 
 
+def passlyrics(filename):
+    with open(filename, 'wr') as sourcefile:
+        for line in sourcefile:
+            if line.strip():
+                sourcefile.write(line)
+
+
 def index_lyrics(filename, score):
     with open('INDEX.db', 'a') as mainindex:
         mainindex.writelines(str(score) + ' ' + filename + '\n')
@@ -24,5 +32,9 @@ def index_lyrics(filename, score):
 if __name__ == '__main__':
     td = os.listdir(DATAPATH)
     for d in td:
-        passfile(d)
-        index_lyrics(d, get_sentiment(DATAPATH+d))
+        if sys.argv[1] == 'pass':
+            passfile(d)
+        else if sys.argv[1] == 'lyrics':
+            passlyrics(DATAPATH+d)
+        else:
+            index_lyrics(d, get_sentiment(DATAPATH+d))
