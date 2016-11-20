707094f9861be58be14d5913855cdeb988b9152a
Alexandru-Paul Copil <alexandru-paul.copil@student.manchester.ac.uk>
2016-11-20 08:05:51 +0000

    Working indexer, issue with UTF-8.

--- indexer.py
+++ indexer.py
@@ -1,12 +1,28 @@
 import os
-import makeuplink
+import unicodedata
+from makeuplink import get_sentiment
+
+DATAPATH = 'youtubeScraper/ok/'
+
+
+def normalize(value):
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
+    value = unicode(re.sub('[-\s]+', '-', value))
+    return value
+
+
+def passfile(filename):
+    newfilename = normalize(filename)
+    os.rename(filename, newfilename)
 
 
 def index_lyrics(filename, score):
     with open('INDEX.db', 'a') as mainindex:
-        mainindex.write(str(score) + ' ' + filename + '\n')
+        mainindex.writelines(str(score) + ' ' + filename + '\n')
 
-
-td = os.listdir('train_data')
-for d in td:
-    index_lyrics(d, 0)
+if __name__ == '__main__':
+    td = os.listdir(DATAPATH)
+    for d in td:
+        passfile(DATAPATH+d)
+        index_lyrics(d, 0)
