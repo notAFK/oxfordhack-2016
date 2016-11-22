5212b6a32b02448523c0a41f23b0675626cc9404
Alexandru-Paul Copil <alexandru-paul.copil@student.manchester.ac.uk>
2016-11-22 11:25:09 +0000

    Minor improvements to indexer.py
    Changed INDEX from .db to .json

--- indexer.py
+++ indexer.py
@@ -5,66 +5,92 @@ import json
 import unicodedata
 from makeuplink import get_sentiment
 
-DATAPATH = 'youtubeScraper/ok/'
-MIDIPATH = 'youtubeScraper/midi/'
+# Point to the path of the youtube scrapper.
+# 'ok'      - folder for lyrics.
+# 'midi'    - folder for midi files.
+LYRI_PATH = 'youtubeScraper/ok/'
+MIDI_PATH = 'youtubeScraper/midi/'
 
 
-def normalize(value):
-    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
-    value = unicode(re.sub('[-\s]+', '-', value))
-    return value
+# Remove any unwanted char from the file name.
+def normalize(text):
+    text = unicode(re.sub('[^\w\s-]', '', text).strip().lower())
+    text = unicode(re.sub('[-\s]+', '-', text))
+    return text
 
 
-def passfile(filename):
+# Apply filename changes to a file.
+def parse_file(filename, path):
+    print '-- PARSING FILE NAME: ' + filename
     newfilename = normalize(filename)
-    os.rename(DATAPATH+filename, DATAPATH+newfilename)
+    os.rename(path+filename, path+newfilename)
 
 
-def passmidi(filename):
-    newfilename = normalize(filename)
-    os.rename(MIDIPATH+filename, MIDIPATH+newfilename)
-
-
-def passlyrics(filename):
-    NEWFILE = ''
+# Remove any white spaces or empty lines.
+def parse_lyri(filename):
+    print '-- PARSING LYRICS: ' + filename
+    newlyrics = ''
+    # Check each line.
     with open(filename, 'r') as sourcefile:
         for line in sourcefile:
             if line.strip():
-                NEWFILE += line
-                print line
-                print 'STRIPE!'
+                newlyrics += line
+    # Write final version of lyri file.
     with open(filename, 'w') as sourcefile:
-        sourcefile.write(NEWFILE)
+        sourcefile.write(newlyrics)
 
 
+# Create entries in the INDEX.json database.
+# Takes a lyrics file and a sentiment score.
 def index_lyrics(filename, score):
+    print '-- INDEXING FILE: ' + filename
+    # Initialize empty json object.
     jsonstring = {}
-    sentiment = ''
-    if score > 0.5:
-        sentiment = 'hpy'
-    else:
-        sentiment = 'sad'
-    midiname = ''
-    for midi in os.listdir(MIDIPATH):
-        if str(midi).startswith(filename[0:8]):
-            midiname = str(midi)
-    jsonstring.update({'score': score, 'filename': filename, 'sentiment': sentiment, 'midi': MIDIPATH+midiname})
 
-    with open('INDEX.db', 'a') as mainindex:
-        mainindex.write(json.dumps(jsonstring))
-        mainindex.write('\n')
+    # Assign a sentiment.
+    sentiment = 'hpy' if score > 0.5 else 'sad'
+
+    # Go trough the midi directory.
+    midiname = 'null'
+    for midi in os.listdir(MIDI_PATH):
+        if str(midi) == filename:
+            midiname = str(midi)
+
+    # Add data to the json object as a dictionary.
+    if midiname == 'null':
+        print '--- No midi file found for ' + filename
+    else:
+        print '--- JSON: ' + jsonstring
+        jsonstring.update({'score': score, 'filename': filename, 'sentiment': sentiment, 'midi': MIDI_PATH+midiname})
+
+        # Write entry to the INDEX.json
+        with open('INDEX.json', 'a') as mainindex:
+            print '--- Wrote JSON to INDEX'
+            mainindex.write(json.dumps(jsonstring))
+            mainindex.write('\n')
+
 
 if __name__ == '__main__':
-    td = os.listdir(DATAPATH)
+    # Load file names from main paths.
+    lyri_list = os.listdir(LYRI_PATH)
+    midi_list = os.listdir(MIDI_PATH)
+
+    # Check for extra arguments.
     if len(sys.argv) > 1:
-        if sys.argv[1] == 'midi':
-            for d in os.listdir(MIDIPATH):
-                passmidi(d)
-    for d in td:
-        if len(sys.argv) > 1:
-            if sys.argv[1] == 'pass':
-                passfile(d)
-            elif sys.argv[1] == 'lyrics':
-                passlyrics(DATAPATH+d)
-        else:
-            index_lyrics(d, get_sentiment(DATAPATH+d))
+        # Parse midi file names.
+        if 'midi' in sys.argv:
+            print '\n- START PARSING MIDI FILES'
+            for midi in midi_list:
+                parse_file(midi, MIDI_PATH)
+        # Parse lyrics file names and content.
+        if 'lyri' in sys.argv:
+            print '\n- START PARSING LYRI FILES'
+            for lyri in lyri_list:
+                parse_file(lyri, LYRI_PATH)
+                parse_lyri(LYRI_PATH + lyri)
+        # Write entries to .json and get sentiment from MS Text An. API
+        if 'index' in sys.argv:
+            print '\n- START INDEXING'
+            for lyri in lyri_list:
+                # index_lyrics(lyri, get_sentiment(LYRI_PATH+lyri))
+                index_lyrics(lyri, 0)
